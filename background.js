81b4426aff988e6e7cef3301a8cc3e3029a12d40
richardhayes <rhshadowgreen@gmail.com>
2018-02-17 15:33:52 -0500

    update

--- background.js
+++ background.js
@@ -2,25 +2,15 @@
 // main events
 // ***********
 
+let EXT_ID = 'oceibmiohadejaominokfpmimceifdmn';
+
 chrome.browserAction.onClicked.addListener(function(activeTab) {
     // when the extension icon is clicked launch the extensions
     // main page
     //
 
-    /*
-    chrome.storage.local.get("blocker", function (obj) {
-
-        console.log(obj);
-
-        if (obj == {}) { // value is unset
-            //chrome.storage.local.set({'blocker': state});
-            console.log("not defined - blocker");
-        }
-        console.log("defined - blocker");
-    }); */
-
     //initStorage(true);
-    mainMenu(true); // true requests new tab
+    //mainMenu(true); // true requests new tab
 });
 
 chrome.webRequest.onBeforeRequest.addListener(function(evt) {
@@ -47,10 +37,10 @@ function isWhiteList(url) {
     //
 
     let whiteList = [
-        "oceibmiohadejaominokfpmimceifdmn", // app id
-        "fonts",                            // css google font
-        "style.css",                        // extension styles
-        "chrome-extension"                  // extension pages
+        EXT_ID,             // app id
+        "fonts",            // css google font
+        "style.css",        // extension styles
+        "chrome-extension"  // extension pages
     ];
 
     for (let i=0; i<whiteList.length; i++) {
@@ -62,12 +52,12 @@ function isWhiteList(url) {
 function mainMenu(newTab) {
 
     if (newTab) {
-        var _url = "chrome-extension://oceibmiohadejaominokfpmimceifdmn/main.html";
+        var _url = "chrome-extension://"+ EXT_ID +"/main.html";
         chrome.tabs.create({ url: _url });
 
     } else {
         chrome.tabs.query({currentWindow: true, active: true}, function (tab) {
-              chrome.tabs.update(tab.id, {url: "chrome-extension://oceibmiohadejaominokfpmimceifdmn/main.html"});
+              chrome.tabs.update(tab.id, {url: "chrome-extension://"+ EXT_ID +"/main.html"});
         });
     }
 }
@@ -75,17 +65,6 @@ function mainMenu(newTab) {
 //
 // storage
 //
-
-function initStorage(state) {
-    // ensures storage value is init
-    //
-
-    chrome.storage.local.get("blocker", function (obj) {
-        if (obj == {}) { // value is unset
-            chrome.storage.local.set({'blocker': state});
-        }
-    });
-}
 
 function getBlockState(callback) {
     // returns true if pages should be blocked
@@ -100,8 +79,15 @@ function toggleBlockState() {
     // toggle block state
     //
 
-    chrome.storage.local.get("blocker", function (value) {
-        chrome.storage.local.set({'blocker': !value});
+    chrome.storage.local.get("blocker", function (obj) {
+
+        if (obj) {
+            chrome.storage.local.set({'blocker': false});
+
+        } else {
+            chrome.storage.local.set({'blocker': true});
+        }
+
     });
 }
 
