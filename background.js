fc9f05c71ab8d9d86b8783926a9481150a486409
richardhayes <rhshadowgreen@gmail.com>
2018-02-17 14:15:57 -0500

    working blocker and main app page

--- background.js
+++ background.js
@@ -3,10 +3,44 @@ chrome.browserAction.onClicked.addListener(function(activeTab) {
     chrome.tabs.create({ url: _url });
 });
 
-// TODO - get something that can detect when the user tries to navigate
+chrome.webRequest.onBeforeRequest.addListener(function(evt) {
+    //
+    // intercepts all browser requests and cancels them
+    //
 
-chrome.webRequest.onBeforeRequest.addListener(function() {
+    // block all pages except for ui-pages belonging to this extension
+    if ( !isWhiteList(evt.url) ) {
 
-    return {cancel: true};
+        chrome.tabs.query({currentWindow: true, active: true}, function (tab) {
+              chrome.tabs.update(tab.id, {url: "chrome-extension://oceibmiohadejaominokfpmimceifdmn/main.html"});
+        });
+
+        // return {cancel: true}; // not required because of re-direction
+    }
 
 }, {urls: ["<all_urls>"]}, ['blocking']);
+
+//
+// helper functions
+//
+function loadMain() {
+    chrome.tabs.getCurrent(function (tab) {
+        console.log(tab.id);
+        chrome.tabs.update(tab.id, {url: "chrome-extension://oceibmiohadejaominokfpmimceifdmn/main.html"});
+    });
+}
+
+function isWhiteList(url) {
+    // check if this url is on the white list
+
+    let whiteList = [
+        "oceibmiohadejaominokfpmimceifdmn",
+        "fonts",
+        "style.css"
+    ];
+
+    for (let i=0; i<whiteList.length; i++) {
+        if (url.indexOf(whiteList[i]) != -1){ return true; }
+    }
+    return false;
+}
