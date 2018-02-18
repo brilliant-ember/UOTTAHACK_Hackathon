6b2b236a507a7325a014b8eb2bb543222f9b8097
richardhayes <rhshadowgreen@gmail.com>
2018-02-18 08:30:21 -0500

    changed timer to minutes

--- options.js
+++ options.js
@@ -15,11 +15,14 @@ function restore_options() {
 
     // Use default value blocker = true
     chrome.storage.sync.get({
-        blocker: true
+        blocker: true,
+        timer: false
 
     }, function(value) {
-        chrome.storage.sync.set({blocker: value});
+        chrome.storage.sync.set({blocker: value['blocker']});
+        chrome.storage.sync.set({blocker: value['timer']});
         blockingState(value['blocker']);
+        timerState(value['timer']);
 
     });
 }
@@ -49,24 +52,47 @@ function blockingState(value) {
     document.getElementById("title-h1").innerHTML = options.title;
 }
 
+function timerState(state) {
+
+    if (state) {
+        document.getElementById("timer-text").style.visibility  = 'hidden';
+        document.getElementById("timer-input").style.visibility = 'hidden';
+        document.getElementById("go-button").style.visibility   = 'hidden';
+        document.getElementById("timer-word").style.visibility  = 'hidden';
+
+    } else {
+        document.getElementById("timer-text").style.visibility  = 'visible';
+        document.getElementById("timer-input").style.visibility = 'visible';
+        document.getElementById("go-button").style.visibility   = 'visible';
+        document.getElementById("timer-word").style.visibility  = 'visible';
+
+    }
+}
+
 function activate_timer() {
     // hides toggle button and starts timer
     // preventing user from turining off
     // until seconds have passed
     //
 
-    document.getElementById("title-button").style.visibility = 'hidden';
-    chrome.storage.sync.set({blocker: true});
-    blockingState(true);
+    let timestring = document.getElementById("timer-input").value;
 
-    let seconds = parseInt(document.getElementById("seconds-input").value) * 1000;
-    document.getElementById("seconds-input").value = "";
+    if (timestring != "") {
+        let time = parseInt(timestring) * 60000;
+        document.getElementById("timer-input").value = "";
 
-    setTimeout(function() {
-        document.getElementById("title-button").style.visibility = 'visible';
-        chrome.storage.sync.set({blocker: false});
-        blockingState(false);
-    }, seconds);
+        document.getElementById("title-button").style.visibility = 'hidden';
+        chrome.storage.sync.set({blocker: true});
+        blockingState(true);
+        timerState(true);
+
+        setTimeout(function() {
+            document.getElementById("title-button").style.visibility = 'visible';
+            chrome.storage.sync.set({blocker: false});
+            blockingState(false);
+            timerState(false);
+        }, time);
+    }
 }
 
 document.addEventListener('DOMContentLoaded', restore_options);
