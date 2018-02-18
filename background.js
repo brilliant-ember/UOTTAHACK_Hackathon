dd4e16637e18c61b08fc22da5b97b9b228525541
brilliant-ember <fire.senfony@gmail.com>
2018-02-17 20:16:15 -0600

    working ish version

--- background.js
+++ background.js
@@ -7,15 +7,13 @@ chrome.webRequest.onBeforeRequest.addListener(function(evt) {
     // intercept all browser requests and cancel them
     // redirect user to the extension main page
 
-    deActivate_Listner();
-    Activate_Listner();
-    
-    if(state){
+
+
     chrome.tabs.query(
         {currentWindow: true, active: true}, 
         function (tab) {
               chrome.tabs.update(tab.id, {url: "https://www.google.ca"});})
-    }
+    
 
     
 }, {urls: ["<all_urls>"]}, ['blocking']);
--- popup.js
+++ popup.js
@@ -4,6 +4,20 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
+chrome.webRequest.onBeforeRequest.addListener(function(evt) {
+    // intercept all browser requests and cancel them
+    // redirect user to the extension main page
+
+
+
+    chrome.tabs.query(
+        {currentWindow: true, active: true}, 
+        function (tab) {
+              chrome.tabs.update(tab.id, {url: "https://www.google.ca"});})
+    
+
+    
+}, {urls: ["<all_urls>"]}, ['blocking']);
 
 
 function main() {
